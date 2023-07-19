33c6144c4db554aa879ec0bf1792083731d4e187
ThomasCode92 <thomascode92@hotmail.com>
2023-07-19 18:45:32 +0200

    Fetching Accounts from Ganache

--- tests/inbox.test.js
+++ tests/inbox.test.js
@@ -3,3 +3,18 @@ const { Web3 } = require('web3');
 
 const { provider } = ganache;
 const web3 = new Web3(provider());
+
+beforeEach(done => {
+  // Get a list of all accounts
+  web3.eth.getAccounts().then(fetchedAccounts => {
+    console.log(fetchedAccounts);
+    done();
+  });
+
+  // Use one of those accounts to deploy
+  // the contract
+});
+
+describe('Inbox Contract', () => {
+  test('should deploy a contract', () => {});
+});
