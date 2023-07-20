650e1fcc677fe8c4e35b68c066867a3fe3c0e618
ThomasCode92 <thomascode92@hotmail.com>
2023-07-20 07:43:45 +0200

    Deployment with Web3

--- tests/inbox.test.js
+++ tests/inbox.test.js
@@ -1,18 +1,23 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 
+const { abi, evm } = require('../compile');
+
 const { provider } = ganache;
 const web3 = new Web3(provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-  console.log(accounts);
 
   // Use one of those accounts to deploy
   // the contract
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox Contract', () => {
